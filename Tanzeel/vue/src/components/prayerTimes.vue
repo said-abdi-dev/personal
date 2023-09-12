c3826a79217a7c8941ea9c83b129300098b7d256
Said Abdi <114350654+said-abdi-dev@users.noreply.github.com>
2023-09-12 11:55:35 -0400

    pushing

--- Tanzeel/vue/src/components/prayerTimes.vue
+++ Tanzeel/vue/src/components/prayerTimes.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="home">
-    <section class="greetings" id="greetings">
-      <h1 class="arabic-greeting">السلام عليكم ورحمة الله وبركاته</h1>
-      <h1>Welcome to Tanzeel</h1>
-    </section>
-  
-    <section class="prayer-times" id="prayer-times">
-      <div class="salah-time">
-        <h1 class="prayer-time-title" >Prayer Times</h1>
-        <ul>
-          <li v-for="(time, prayer) in prayerTimes" :key="prayer">
-            {{ prayer }}: {{ time }}
-          </li>
-        </ul>
+  <div class="prayer-times-container">
+    <h1 class="city-name">{{ cityName }}</h1>
+    
+    <!-- Prayer times section -->
+    <div class="prayer-time-card">
+      <div class="prayer-time-heading">Fajr</div>
+      <div class="prayer-time">{{ formatTimeTo12HourClock(prayerTimes.Fajr) }}</div>
+    </div>
+    <div class="prayer-time-card">
+      <div class="prayer-time-heading">Dhuhr</div>
+      <div class="prayer-time">{{ formatTimeTo12HourClock(prayerTimes.Dhuhr) }}</div>
+    </div>
+    <div class="prayer-time-card">
+      <div class="prayer-time-heading">Asr</div>
+      <div class="prayer-time">{{ formatTimeTo12HourClock(prayerTimes.Asr) }}</div>
+    </div>
+    <div class="prayer-time-card">
+      <div class="prayer-time-heading">Maghrib</div>
+      <div class="prayer-time">{{ formatTimeTo12HourClock(prayerTimes.Maghrib) }}</div>
+    </div>
+    <div class="prayer-time-card">
+      <div class="prayer-time-heading">Isha</div>
+      <div class="prayer-time">{{ formatTimeTo12HourClock(prayerTimes.Isha) }}</div>
+    </div>
+    
+    <!-- Location selection section -->
+    <section class="location-selection">
+      <h2>Select Location</h2>
+      <div class="location-form">
+        <label for="city">City:</label>
+        <input type="text" id="city" v-model="location.city" placeholder="Enter city">
+        
+        <label for="country">Country:</label>
+        <input type="text" id="country" v-model="location.country" placeholder="Enter country">
+        
+        <button @click="fetchPrayerTimes">Get Prayer Times</button>
       </div>
     </section>
-    
+    <!--Foooter section -->
     <section class="footer" id="footer">
       <div class="creator-info">
         <p>Created by: Said Abdi</p>
@@ -31,20 +53,15 @@ import PrayerTimeService from '@/services/PrayerTimeService';
 import GoogleGeocodingService from '@/services/GoogleGeocodingService';
 
 export default {
+  props: {
+    cityName: String,
+  },
   data() {
     return {
       prayerTimes: {},
       location: {
         city: '',
         country: '',
-      },
-      salahTimes: {
-        fajr: '',
-        sunrise: '',
-        Dhuhr: '',
-        Asr: '',
-        Maghrib: '',
-        Isha: '',
       },
     };
   },
@@ -103,74 +120,104 @@ export default {
       const now = new Date();
       return now.toISOString().slice(0, 10);
     },
+    formatTimeTo12HourClock(time24) {
+      const [hour24, minute] = time24.split(':');
+      const hour12 = (parseInt(hour24, 10) % 12) || 12;
+      const ampm = parseInt(hour24, 10) < 12 ? 'AM' : 'PM';
+      return `${hour12}:${minute} ${ampm}`;
+    },
+    fetchPrayerTimes() {
+      const { city, country } = this.location;
+      // Implement your fetch prayer times logic here based on the selected location (city and country).
+      // Update this.prayerTimes with the new data.
+    },
   },
-  
 };
 </script>
 
 <style scoped>
-.home {
-  background-color: #024422;
-  min-height: 100vh;
+/* Styling for both sections */
+.prayer-times-container {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-
+  padding: 20px;
+  background-color: #5C8374;
+  border-radius: 8px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  text-align: center;
 }
 
-h1 {
-  color: #5C8374;
-}
-
-p {
-  color: #93B1A6;
-}
-
-ul li {
-  color: #5C8374;
-}
-
-.footer {
-  background-color: #24a3e7;
-}
-.prayer-time-title {
+.city-name {
   font-size: 24px;
+  margin-bottom: 10px;
+  color: #333;
 }
 
-.creator-info p {
-  color: #93B1A6;
+.prayer-time-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px 0;
+  padding: 10px;
+  background-color: #f4f4f4;
+  border-radius: 5px;
+  width: 200px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
 }
 
-.creator-info a {
-  color: #183D3D;
+.prayer-time-heading {
+  font-size: 18px;
+  font-weight: bold;
+  color: #333;
 }
 
-.creator-info a:hover {
-  color: #5C8374;
-}
-
-.arabic-greeting {
+.prayer-time {
   font-size: 20px;
-  animation: slideIn 1s ease-in-out;
+  color: #555;
 }
 
-@keyframes slideIn {
-  0% {
-    transform: translateX(-100%);
-    opacity: 0;
-  }
-  100% {
-    transform: translateX(0);
-    opacity: 1;
-  }
+/* Styling for location selection section */
+.location-selection {
+  text-align: center;
+  margin-top: 20px;
 }
 
-html {
-  scroll-behavior: smooth;
+.location-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.navigation {
-  display: none;
+label {
+  margin-bottom: 5px;
+}
+
+input {
+  padding: 5px;
+  margin-bottom: 10px;
+}
+
+button {
+  background-color: #183D3D;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #5C8374;
+}
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: red;
+  color: white;
+  text-align: center;
 }
 </style>
+
